ee3b26d67999cb63e90d1cafdcf4162561fd7e26
Joshua Jansen <joshua@itflows.nl>
2012-10-29 16:26:52 +0100

    Added beer icon

---
+++ barmanager/BarIndexViewController.h
@@ -0,0 +1,13 @@
+//
+//  BarIndexViewController.h
+//  barmanager
+//
+//  Created by Joshua Jansen on 29-10-12.
+//  Copyright (c) 2012 ITflows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BarIndexViewController : UITableViewController
+
+@end
